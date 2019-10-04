d4736b276f684470517dc0af851309f3eaa86a98
Saeed Sarpas <saeed.sarpas@phys.ethz.ch>
2019-10-04 20:21:37 +0200

    Initializing PyRhyme object by loading the given chombo file

--- py_rhyme/tests/chombo.py
+++ py_rhyme/tests/chombo.py
@@ -5,15 +5,17 @@ import numpy as np
 
 GRID = [16, 16, 16]
 LENGTHS = [1.0, 1.0, 1.0]
-FILENAME = 'py_rhyme/tests/assets/3d.chombo.h5'
+FIELDS = ( 'field_0', 'field_1' )
+NUM_COMPONENTS = len(FIELDS)
+ITERATION = 123
+TIME = 2.34e5
+DIMENTIONALITY = 3
+NUM_LEVELS = 1
+TEST_CHOMBO_PATH = 'py_rhyme/tests/assets/3d.chombo.h5'
 
 
 def create():
     """Creating a chombo file based on above constants."""
-
-    grid = GRID
-    lengths = LENGTHS
-    filename = FILENAME
 
     box_dtype = [
         ('lo_i', np.int),
@@ -24,44 +26,42 @@ def create():
         ('hi_k', np.int),
     ]
 
-    chombo = h5py.File(filename, 'w')
+    chombo = h5py.File(TEST_CHOMBO_PATH, 'w')
 
-    chombo.attrs['ProblemDomain'] = np.array(grid, dtype=np.int)
+    chombo.attrs['ProblemDomain'] = np.array(GRID, dtype=np.int)
 
-    chombo.attrs['component_0'] = np.string_('field_0')
-    chombo.attrs['component_1'] = np.string_('field_1')
-    chombo.attrs['num_levels'] = 1
-    chombo.attrs['num_components'] = 2
+    for i, f in enumerate(FIELDS):
+        chombo.attrs['component_%d' % i] = np.string_(FIELDS[i])
 
-    chombo.attrs['iteration'] = 1
-    chombo.attrs['time'] = 1.23e4
+    chombo.attrs['num_levels'] = NUM_LEVELS
+    chombo.attrs['num_components'] = NUM_COMPONENTS
+
+    chombo.attrs['iteration'] = ITERATION
+    chombo.attrs['time'] = TIME
 
     chombo_global = chombo.create_group('Chombo_global')
-    chombo_global.attrs['SpaceDim'] = [3]
+    chombo_global.attrs['SpaceDim'] = DIMENTIONALITY
 
     # level 0
     lev_0 = chombo.create_group('level_0')
-    box_0 = ( 0, 0, 0, grid[0] - 1, grid[1] - 1, grid[2] - 1 )
-    data_0_field_0 = [
-        1 * 1e2 + i * 1 + j * 1e-2 + k * 1e-4
-        for i in range(grid[0])
-        for j in range(grid[1])
-        for k in range(grid[2])
-    ]
-    data_0_field_1 = [
-        2 * 1e2 + i * 1 + j * 1e-2 + k * 1e-4
-        for i in range(grid[0])
-        for j in range(grid[1])
-        for k in range(grid[2])
-    ]
+    box_0 = ( 0, 0, 0, GRID[0] - 1, GRID[1] - 1, GRID[2] - 1 )
+    data_0 = []
 
-    lev_0.attrs['dx'] = [ l / g for l, g in zip(lengths, grid) ]
-    lev_0.attrs['prob_domain'] = np.array([box_0], dtype=box_dtype)
+    for i in range(len(FIELDS)):
+        data_0.append([
+            i * 1e2 + i * 1 + j * 1e-2 + k * 1e-4
+            for i in range(GRID[0])
+            for j in range(GRID[1])
+            for k in range(GRID[2])
+        ])
+
+    lev_0.attrs['dx'] = [ l / g for l, g in zip(LENGTHS, GRID) ]
+    lev_0.attrs['prob_domain'] = box_0 # np.array([box_0], dtype=box_dtype)
     lev_0.attrs['ref_ratio'] = [2.0]
 
     boxes_0 = lev_0.create_dataset('boxes', (1,), dtype=box_dtype)
     boxes_0[0] = box_0
 
-    lev_0['data:datatype=0'] = data_0_field_0 + data_0_field_1
+    lev_0['data:datatype=0'] = sum(data_0, [])
 
     chombo.close()
--- py_rhyme/tests/main_test.py
+++ py_rhyme/tests/main_test.py
@@ -1,5 +1,25 @@
 from ..main import PyRhyme
-import py_rhyme.tests.chombo as chombo
+from py_rhyme.tests.chombo import *
 
-def test_if_main_works_properly():
-    pass
+def test_main_init():
+    rhyme = PyRhyme(TEST_CHOMBO_PATH)
+
+    assert (rhyme.snap['attr']['problem_domain'] == GRID).all()
+
+    assert rhyme.snap['attr']['num_levels'] == NUM_LEVELS
+    assert rhyme.snap['attr']['num_components'] == NUM_COMPONENTS
+
+    for i in range(NUM_COMPONENTS):
+        assert rhyme.snap['attr']['component_%d' % i] == FIELDS[i]
+
+    assert rhyme.snap['attr']['iteration'] == ITERATION
+    assert rhyme.snap['attr']['time'] == TIME
+
+    assert rhyme.snap['levels'][0]['dx'] == tuple([ l / g for l, g in zip(LENGTHS, GRID)])
+    assert rhyme.snap['levels'][0]['ref_ratio'] == 2.0
+
+    assert rhyme.snap['levels'][0]['boxes']['max'] == 0
+    assert rhyme.snap['levels'][0]['boxes'][0]['corner'][3:6] == tuple([ g - 1 for g in GRID ])
+    assert len(rhyme.snap['levels'][0]['boxes'][0]['offset']) == NUM_COMPONENTS
+
+    assert len(rhyme.snap['levels'][0]['data']) == len(FIELDS) * GRID[0] * GRID[1] * GRID[2]
